644889d6e377c11005c0a2129651c0b14a519233
ramirezfred <ramirez.fred@hotmail.com>
2026-05-21 15:44:59 -0400

    visualizacion de servicios

--- src/app/components/service-card/service-card.component.ts
+++ src/app/components/service-card/service-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 export interface SocialLink {
   platform: string;
   url: string;
   icon: string;
+  safeIcon?: SafeHtml;
 }
 
 @Component({
@@ -16,9 +18,36 @@ export interface SocialLink {
   styleUrl: './service-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ServiceCardComponent {
+export class ServiceCardComponent implements OnChanges, OnInit {
   @Input({ required: true }) iconSvg!: string;
   @Input({ required: true }) titleKey!: string;
   @Input({ required: true }) descriptionKey!: string;
   @Input() socialLinks: SocialLink[] = [];
+
+  safeIconSvg!: SafeHtml;
+  private readonly sanitizer = inject(DomSanitizer);
+
+  ngOnInit(): void {
+    this.updateIcons();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateIcons();
+  }
+
+  private updateIcons(): void {
+    if (this.iconSvg) {
+      this.safeIconSvg = this.sanitizer.bypassSecurityTrustHtml(this.iconSvg);
+    }
+    
+    if (this.socialLinks && this.socialLinks.length > 0) {
+      // Create a new array with safeIcon properties
+      this.socialLinks = this.socialLinks.map(link => {
+        return link.safeIcon ? link : {
+          ...link,
+          safeIcon: this.sanitizer.bypassSecurityTrustHtml(link.icon)
+        };
+      });
+    }
+  }
 }
