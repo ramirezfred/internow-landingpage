b042ab7ec4153fa1b0227c48e100d54a9f300c21
ramirezfred <ramirez.fred@hotmail.com>
2026-05-29 15:27:07 -0400

    enlaces a redes y tiendas

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -48,6 +48,23 @@ export class ServicesComponent implements AfterViewInit {
     { platform: 'Facebook', url: 'https://www.facebook.com/profile.php?id=61576521206113', icon: this.fbIcon }
   ];
 
+  readonly chinaLinks: SocialLink[] = [
+    { platform: 'Facebook', url: 'https://www.facebook.com/share/1EpcD1o3QV/', icon: this.fbIcon }
+  ];
+
+  readonly consultingLinks: SocialLink[] = [
+    { platform: 'Facebook', url: 'https://www.facebook.com/profile.php?id=100092537898574', icon: this.fbIcon },
+    { platform: 'Instagram', url: 'https://www.instagram.com/internowmex?igsh=MXU3amU4cHR2YWJ0NA==', icon: this.igIcon }
+  ];
+
+  readonly playStoreIcon = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M5 21V3l14 9-14 9z"/><path d="M5 3l9 9-9 9"/><path d="M19 12H5"/></svg>`;
+  readonly appleIcon = `<svg viewBox="0 0 24 24" fill="currentColor"><path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.81-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M15.97 4.17c.66-.81 1.11-1.93.99-3.06-1 .04-2.22.67-2.94 1.51-.64.74-1.2 1.88-1.05 3 .12.09.24.14.36.14.88 0 2.02-.6 2.64-1.59z"/></svg>`;
+
+  readonly appLinks: SocialLink[] = [
+    { platform: 'Google Play', url: 'https://play.google.com/store/apps/dev?id=5638693745199999314', icon: this.playStoreIcon },
+    { platform: 'App Store', url: 'https://apps.apple.com/mx/developer/internow-corp-sa-de-cv/id1629327171', icon: this.appleIcon }
+  ];
+
   ngAfterViewInit(): void {
     this.initScrollReveal();
   }
